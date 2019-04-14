8d61d8c209c5d40b8a1c73c5a3c508e16979977a
Rıdvan Uyan <rduyan@ebay.com>
2019-04-14 03:55:40 +0300

    eklenmeyen saçmalıklar

---
+++ Assets/Vuforia/Scripts/textSaglamKart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textSaglamKart : MonoBehaviour
+{
+	TextMesh textMesh;
+	KuveytApi cardInfo = new KuveytApi();
+
+	void Start()
+	{
+		CardInformation card = cardInfo.getCreditCardInformation("4025916319964789");
+		textMesh = GetComponent<TextMesh>();
+		//textMesh.text = card.Limit + " TL \n" + card.PointAmount + " TL \n" + card.InstallmentCount + " TL";
+        textMesh.text = " fdsfdasfdsa TL \n TLfdsfadsfdsa \n fdsfdsaTL";
+
+
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
