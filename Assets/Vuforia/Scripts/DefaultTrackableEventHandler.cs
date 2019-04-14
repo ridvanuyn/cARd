fa7a49db38bac1b49fb6984bf6af01a679403eb9
Ömer Taşkın <otaskin@ebay.com>
2019-04-14 09:02:42 +0300

    bestchoice impl

--- Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -164,3 +164,18 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
 
     #endregion // PROTECTED_METHODS
 }
+
+sealed class ReverseComparer<T> : IComparer<T>
+{
+    private readonly IComparer<T> original;
+
+    public ReverseComparer(IComparer<T> original)
+    {
+        this.original = original;
+    }
+
+    public int Compare(T left, T right)
+    {
+        return original.Compare(right, left);
+    }
+}
